50628963dbe01b10cb089ab718a0d1297a64a849
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2023-07-14 17:24:42 +0000

    fix: i forgot to remove some other stuff

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ git pull
 
 docker build . -t srizan10/sernlinear
 
-docker stop vinci
+docker stop sern-linear
 
-docker rm vinci
+docker rm sern-linear
 
 docker run -d -t --name sern-linear --restart unless-stopped srizan10/sernlinear
